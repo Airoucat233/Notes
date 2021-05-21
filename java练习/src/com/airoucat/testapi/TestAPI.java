ecedc3500c26ea99cee4a26144f8df8379fd1301
airoucat <529401274@qq.com>
2021-05-21 17:52:29 +0800

    ss

--- java练习/src/com/airoucat/testapi/TestAPI.java
+++ java练习/src/com/airoucat/testapi/TestAPI.java
@@ -1,2 +1,24 @@
-package com.airoucat.testapi;public class TestAPI {
+package com.airoucat.testapi;
+
+public class TestAPI {
+    public static void main(String[]args){
+        try {
+
+            RestUtil restUtil = new RestUtil();
+
+            String resultString = restUtil.load(
+                    "http://www.anjismart.com:7770/QueryCodeService.asmx/QueryCode?code=425144174493800&aid=302",
+                    "");
+
+        } catch (Exception e) {
+
+            // TODO: handle exception
+
+            System.out.print(e.getMessage());
+
+        }
+    }
 }
+
+
+
